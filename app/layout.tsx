8cfc448bb287e0e55f01ac59b318a7f7a9727370
Dikeh Daniel <dikehdaniel2020@gmail.com>
2026-07-01 23:44:01 -0700

    add: font style

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body className="antialiased">
         <AuthSessionProvider>
           <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
             {children}
